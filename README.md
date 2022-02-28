d85f29bcd2da0437f0d1a40ef2e8696c85a448de
MightGit <74358998+MightGit@users.noreply.github.com>
2022-02-28 13:13:08 +0100

    fået en graf til at vise sig. nu mangler vi at brugeren kan give input. derudover virker voeres tkinter. og vi har fået lavet 2 entries til start pg sæutværdi på x.

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Dette er vores projekt hvor vi vil vise et par grafer
 
 Vores [GUI](GUI.PNG) i starten som vi har snakket og diskuteret om ser sådan her ud.
 ![GUI](GUI.PNG)
+
--- graf.py
+++ graf.py
@@ -1,21 +1,39 @@
 from tkinter import *
 from tkinter.ttk import * #progressbar
 from plotter import PlotterClass
+import numpy as np
+import matplotlib.pyplot as plt
+
 class mainWindow:
 
     def __init__(self):
 
+        y1=200
+        x0=50
+        x1=100
         self.root = Tk()
 
         velkomst = Label(self.root, text="Velkommen til GUI")
         velkomst.pack(pady=10)
 
-        # Progress bar widget
-        listButton = Button(self.root,text ="tryk",command = self.test)
-        listButton.pack(padx = 20, pady = 10,side=LEFT)
+        self.x0 = Entry(self.root)
+        self.x0.pack()
+        if len(self.x0.get()) == 0:
+            self.x0.insert(END, 'indsæt din x0 her')
+
+        self.x1 = Entry(self.root)
+        self.x1.pack()
+        if len(self.x1.get()) == 0:
+            self.x1.insert(END, 'indsæt din x1 her')
 
 
-        pltBtn = Button(self.root,text ="plotwin",command = lambda: PlotterClass(self))
+        def f(x0,x1):
+            graph=np.random.normal(x0,x1,y1)
+            plt.hist(graph,50)
+            plt.show()
+
+
+        pltBtn = Button(self.root,text ="plotwin",command = f(x0,x1))
         pltBtn.pack(padx = 20, pady = 10,side=LEFT)
 
         # infinite loop
@@ -23,8 +41,8 @@ class mainWindow:
 
         #det her kører når man trykker luk:
         #afslut()
-    def test(self):
-        pass
+
+
 
 if __name__ == '__main__':
     main = mainWindow()
