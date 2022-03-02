e46dace167c2139e195e531c42255e8575fe325c
uvuvwevwevwe onyetenyevwe ugwemubwem ossas <74357602+chri92g4@users.noreply.github.com>
2022-03-02 08:51:53 +0100

    Vi har fået koden til at virke sådan at vi kan give programmet en funktion og variables aka x0 og x1 hvor den derefter kan plotte det ved hjælp af sympy

--- graf.py
+++ graf.py
@@ -3,6 +3,8 @@ from tkinter.ttk import * #progressbar
 from plotter import PlotterClass
 import numpy as np
 import matplotlib.pyplot as plt
+from sympy.parsing.sympy_parser import parse_expr
+import sympy as sym
 
 class mainWindow:
 
@@ -16,6 +18,13 @@ class mainWindow:
         velkomst = Label(self.root, text="Velkommen til GUI")
         velkomst.pack(pady=10)
 
+
+        self.fx = Entry(self.root)
+        self.fx.pack()
+        if len(self.fx.get()) == 0:
+            self.fx.insert(END, 'indsæt funktion')
+
+
         self.x0 = Entry(self.root)
         self.x0.pack()
         if len(self.x0.get()) == 0:
@@ -26,14 +35,38 @@ class mainWindow:
         if len(self.x1.get()) == 0:
             self.x1.insert(END, 'indsæt din x1 her')
 
+        def getFunctionFromString(inputString='y= x**2'):
 
-        def f(x0,x1):
-            graph=np.random.normal(x0,x1,y1)
-            plt.hist(graph,50)
+            # Tjek efter om brugeren har tastet = ind i.
+            if '=' in inputString:
+                user_input = inputString.split('=')[1]  # Del input strengen op ved '=', og giv anden del tilbage.
+            else:
+                user_input = inputString
+
+            print(f'This is the mathematical function we are about to make into a real python funktion: {user_input}')
+
+            x, y = sym.symbols('x y')
+
+            expr = parse_expr(user_input)
+
+            f = sym.lambdify([x], expr)
+
+            return f
+
+        def plot():
+            fig, ax = plt.subplots()
+
+            f = getFunctionFromString(self.fx.get())
+            x0=int(self.x0.get())
+            x1=int(self.x1.get())
+            X=np.arange(x0,x1,0.1)
+            Y=f(X)
+            ax.plot(X,Y)
+            ax.set_title(f'Plot af funktionen: {self.fx.get()}')
             plt.show()
 
 
-        pltBtn = Button(self.root,text ="plotwin",command = f(x0,x1))
+        pltBtn = Button(self.root,text ="plotwin",command = plot)
         pltBtn.pack(padx = 20, pady = 10,side=LEFT)
 
         # infinite loop
